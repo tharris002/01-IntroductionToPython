f81b469c0ff9ca160d4a21066eeb99ed75efc097
michaelkuznicki <mkuznicki@franciscan.edu>
2024-01-18 03:37:06 -0500

    Finished m1e and m2

---
+++ src/m1e_print_strings.py
@@ -0,0 +1,27 @@
+###############################################################################
+#
+#   Live in Class:
+#
+#   Read the code below. Predict what you think will be printed/displayed
+#   when you run the code.
+#
+#   Then, run this module by clicking the "run" button (it looks like a play 
+#   button) in the top right corner of this window (if you hover over it, you
+#   will see "Run Python File").
+#
+#   This will open a terminal window where you should see the output of
+#   this code. Confirm that the output is what you expected. If it is not
+#   or if you don't see any output, ask questions now.
+#
+#   This module is an example (notice the 'e' in m1e). For example modules,
+#   please read the code then run it. Feel free to modify slightly and 
+#   move on to the next module.
+#
+###############################################################################
+
+print("Hello World")
+print('This is a simple print statement')
+print("one", "two", "three")
+
+print(1 + 5)
+print("1 + 5", "versus", 1 + 5)
---
+++ src/m2_todo_commit_push.py
@@ -0,0 +1,127 @@
+print('Hello, World')
+
+###############################################################################
+#
+#   This line is a COMMENT -- a note to the person reading this file.
+#   When a program runs, it ignores everything from a  # (hash)  mark
+#   to the end of the line with the  #  mark.
+#
+#   We call files that have Python code in them MODULES.
+#   Line 1 of this module (above) prints (displays) the STRING
+#      Hello, World
+#   on the Run window (terminal).
+#
+#   Anything surrounded by quote marks (single or double) is a STRING.
+#
+###############################################################################
+
+###############################################################################
+#
+# TODO: 1.
+#   The TO-DO comment above this one indicates that there is something for
+#   you to do.
+#
+#   Run this module by clicking the "run" button (it looks like a play 
+#   button) in the top right corner of this window
+#
+#   After running, find the Run window (it may have opened at the bottom or
+#   to the right) and confirm that
+#        Hello, World
+#   did indeed get printed (displayed) in that window.
+#
+###############################################################################
+
+###############################################################################
+#
+# TODO: 2.
+#
+#   Notice that TODO's turn blue even inside of comments. This helps you
+#   find them easier when you are looking for what you still need to do.
+#
+#   When you have completed a TO-DO, you should change the word
+#     _TODO_      (ignore the underscores on this line)
+#   to
+#     DONE
+#   Try it now on line 20 above, and note that it is no longer blue.
+#
+#   If you change TODOs to DONEs like this, you can tell when you have
+#   finished all the exercises in a module -- there will be no blue comments.
+#
+#   You have now completed TO-DO #2, so change its TO-DO on line 36 to DONE
+#   (and proceed similarly for all forthcoming TODOs in this course).
+#
+#   There are also many extensions that can help you manage TODOs and comments.
+#   I am currently using one called 'Better Comments' by Aaron Bond that simply
+#   changes the color of certain types of comments.
+#
+###############################################################################
+
+###############################################################################
+#
+# TODO: 3.
+#   Add another   print   statement below the current Line 1 above.
+#   It should print any string that you want.
+#
+#   Test your code by re-running this module, either by proceeding as you did
+#   when you ran this module the first time by pressing the triangle "Play"
+#   button that is on the toolbar at the top right of this window. Look at
+#   the Run window to be sure that your string printed as expected.
+#
+###############################################################################
+
+###############################################################################
+#
+# TODO: 4.
+#   Add yet another   print   statement.
+#   This one should print the *product* of 3,607 and 34,227.
+#   Let the computer do the arithmetic for you.
+#   You do NOT have to use strings for this, so no quotation marks!
+#   This is a bit of a preview for next week but see if you can figure it out
+#   based on the examples in m1e.
+#
+#   TEST your code by re-running this module, then asking someone: What number
+#   did your   print   display for this TO-DO? You can also check your answer
+#   with a calculator. (HINT: It is an INTERESTING number.)  Get help if your
+#   value is wrong.
+#
+###############################################################################
+
+###############################################################################
+#
+# TODO: 5.
+#   Look at the list of files in the Project window (to the left).  Note that
+#   this file (m2_todo_and_commit_push.py) now has a 'U' next to it.
+#
+#   This means that you have made changes to this file which have not yet
+#   been COMMITTED to version control and PUSHED to the cloud.
+#
+#   COMMIT and PUSH your work by:
+#     1. Select the Sourc Control tool on the very left of this window (it 
+#        looks like a forked path of sorts).
+#     2. Type in a commit message indicating what changes you have made.
+#        "Completed m2" would be sufficient since you are done with this module.
+#     2. Select  Commit  beneath the textbox.
+#     3. Select  Sync Changes  which will save your changes to your GitHub repo.
+#
+#   COMMIT adds the changed work to the version control on your computer
+#   and Sync Changes adds the changed work into your Github repository in 
+#   the "cloud".
+#
+#   PyCharm forces you to add a  Commit  message intended to describe
+#   your work to teammates, but until your team project use   Done
+#   or whatever you want for the  Commit  message.
+#
+#   Always PUSH (in addition to the COMMIT) so that your work is backed-up
+#   in the cloud, and also so that your instructor can access it as needed.
+#   If you COMMIT but forget to PUSH, you can subsequently do the PUSH by:
+#      VCS ~ Git ~ Push...
+#
+#   Oh, one more thing:
+#     Do you have any blue bars on the scrollbar-like thing to the right?
+#     If so, click on each blue bar and change its TO-DO to DONE,
+#     then run the file (to make sure you didn't break anything)
+#     and COMMIT-and-PUSH again.
+#
+#   You can COMMIT-and-PUSH as often as you like.  DO IT FREQUENTLY.
+#
+###############################################################################
